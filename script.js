f6910dd13cdacbccbff713b2004415a74ebbb69d
Waqas Shaikh <farazshaikh7837@gmail.com>
2023-12-08 12:16:08 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const mobileMenuButton = document.getElementById('mobile-menu');
+    const navbarLinks = document.getElementById('navbar-links');
+  
+    mobileMenuButton.addEventListener('click', function () {
+      navbarLinks.classList.toggle('show');
+    });
+  });
+  
